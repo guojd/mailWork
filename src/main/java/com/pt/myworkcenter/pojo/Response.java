9ba26529ac5ab5ca2a5084823715710d9367ceb4
gjd8798 <gjd8798@163.com>
2024-08-13 20:24:20 +0800

    实现本地工作路由设置

---
+++ src/main/java/com/pt/myworkcenter/pojo/Response.java
@@ -0,0 +1,42 @@
+package com.pt.myworkcenter.pojo;
+
+import java.util.List;
+
+public class Response {
+    int code=0;
+    int count=0;
+    Object data;
+    String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
